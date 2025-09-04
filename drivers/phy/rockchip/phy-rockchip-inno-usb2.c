f2a6f5ef6167b9184ae23787827e093d6e81dc87
Mathijs Blok <mathijs@cpeb.it>
2025-09-04 15:57:46 +0200

    usb tweaking

--- drivers/phy/rockchip/phy-rockchip-inno-usb2.c
+++ drivers/phy/rockchip/phy-rockchip-inno-usb2.c
@@ -2813,10 +2813,10 @@ static int rv1106_usb2phy_tuning(struct rockchip_usb2phy *rphy)
 
 	if (rockchip_get_cpu_version()) {
 		/* Set Tx HS pre_emphasize strength to 3'b001 */
-		phy_update_bits(rphy->phy_base + 0x40, GENMASK(5, 3), (0x01 << 3));
+		phy_update_bits(rphy->phy_base + 0x40, GENMASK(5, 3), (0x03 << 3));
 	} else {
 		/* Set Tx HS pre_emphasize strength to 3'b011 */
-		phy_update_bits(rphy->phy_base + 0x40, GENMASK(5, 3), (0x01 << 3));
+		phy_update_bits(rphy->phy_base + 0x40, GENMASK(5, 3), (0x03 << 3));
 	}
 
 	/* Set RX Squelch trigger point configure to 4'b0000(112.5 mV) */
