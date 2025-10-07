4e18d464d4f918bb09b2129c7e4844378d7a15eb
Chiel Prins <chiel@cpeb.it>
2025-10-07 13:12:51 +0200

    Revert "Improve USB TX HS eye height tuning to fix CP USB issues"
    This reverts commit d800d32800573a2169f6ac2ae7442cf9b4750c3d.

--- drivers/phy/rockchip/phy-rockchip-inno-usb2.c
+++ drivers/phy/rockchip/phy-rockchip-inno-usb2.c
@@ -2828,8 +2828,8 @@ static int rv1106_usb2phy_tuning(struct rockchip_usb2phy *rphy)
 	/* Set 45ohm HS ODT value to 5'b10111 to increase driver strength */
 	phy_update_bits(rphy->phy_base + 0x11c, GENMASK(4, 0), 0x17);
 
-	/* Set TX HS eye height tuning to 3'b111(~600 mV)*/
-	phy_update_bits(rphy->phy_base + 0x124, GENMASK(4, 2), (0x07 << 2));
+	/* Set Tx HS eye height tuning to 3'b011(462 mV)*/
+	phy_update_bits(rphy->phy_base + 0x124, GENMASK(4, 2), (0x03 << 2));
 
 	/* Bypass Squelch detector calibration */
 	phy_update_bits(rphy->phy_base + 0x1a4, GENMASK(7, 4), (0x01 << 4));
