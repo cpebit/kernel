a7c684012af147b37e0ba789fe4093d578837676
Mathijs Blok <mathijs@cpeb.it>
2025-10-21 10:41:34 +0200

    tryp bigger eye height

--- drivers/phy/rockchip/phy-rockchip-inno-usb2.c
+++ drivers/phy/rockchip/phy-rockchip-inno-usb2.c
@@ -2813,7 +2813,7 @@ static int rv1106_usb2phy_tuning(struct rockchip_usb2phy *rphy)
 
 	if (rockchip_get_cpu_version()) {
 		/* Set Tx HS pre_emphasize strength to 3'b001 */
-		phy_update_bits(rphy->phy_base + 0x40, GENMASK(5, 3), (0x03 << 3));
+		phy_update_bits(rphy->phy_base + 0x40, GENMASK(5, 3), (0x01 << 3));
 	} else {
 		/* Set Tx HS pre_emphasize strength to 3'b011 */
 		phy_update_bits(rphy->phy_base + 0x40, GENMASK(5, 3), (0x03 << 3));
@@ -2829,7 +2829,7 @@ static int rv1106_usb2phy_tuning(struct rockchip_usb2phy *rphy)
 	phy_update_bits(rphy->phy_base + 0x11c, GENMASK(4, 0), 0x17);
 
 	/* Set TX HS eye height tuning to 3'b111(~600 mV)*/
-	phy_update_bits(rphy->phy_base + 0x124, GENMASK(4, 2), (0x07 << 2));
+	phy_update_bits(rphy->phy_base + 0x124, GENMASK(4, 2), (0x05 << 2));
 
 	/* Bypass Squelch detector calibration */
 	phy_update_bits(rphy->phy_base + 0x1a4, GENMASK(7, 4), (0x01 << 4));
