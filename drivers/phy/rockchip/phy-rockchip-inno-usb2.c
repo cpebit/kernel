1a0a84e15c439001f69fb6484614bda8e657b1d0
Chiel Prins <chiel@cpeb.it>
2025-10-22 09:31:41 +0200

    hs disconnect level 1

--- drivers/phy/rockchip/phy-rockchip-inno-usb2.c
+++ drivers/phy/rockchip/phy-rockchip-inno-usb2.c
@@ -2829,7 +2829,7 @@ static int rv1106_usb2phy_tuning(struct rockchip_usb2phy *rphy)
 	phy_update_bits(rphy->phy_base + 0x11c, GENMASK(4, 0), 0x17);
 
 	/* Set Tx HS eye height tuning to 3'b011(462 mV)*/
-	phy_update_bits(rphy->phy_base + 0x124, GENMASK(4, 2), (0x05 << 2));
+	phy_update_bits(rphy->phy_base + 0x124, GENMASK(4, 2), (0x03 << 2));
 
 	/* Bypass Squelch detector calibration */
 	phy_update_bits(rphy->phy_base + 0x1a4, GENMASK(7, 4), (0x01 << 4));
@@ -2837,6 +2837,10 @@ static int rv1106_usb2phy_tuning(struct rockchip_usb2phy *rphy)
 
 	/* Set HS disconnect detect mode to single ended detect mode */
 	phy_set_bits(rphy->phy_base + 0x70, BIT(2));
+
+	phy_update_bits(rphy->phy_base + 0x60, 0x3, 0x0);
+	phy_update_bits(rphy->phy_base + 0x68, 0x1, 0x0);
+	phy_update_bits(rphy->phy_base + 0x64, 0x80, 0x80);
 
 	return 0;
 }
