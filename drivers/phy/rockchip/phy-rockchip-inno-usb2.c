e2f0882fb32300c3134ed986fd60804bc32b1f29
Chiel Prins <chiel@cpeb.it>
2025-11-06 15:58:33 +0100

    hs disconnect level 1

--- drivers/phy/rockchip/phy-rockchip-inno-usb2.c
+++ drivers/phy/rockchip/phy-rockchip-inno-usb2.c
@@ -2838,6 +2838,10 @@ static int rv1106_usb2phy_tuning(struct rockchip_usb2phy *rphy)
 	/* Set HS disconnect detect mode to single ended detect mode */
 	phy_set_bits(rphy->phy_base + 0x70, BIT(2));
 
+	phy_update_bits(rphy->phy_base + 0x60, 0x3, 0x0);
+	phy_update_bits(rphy->phy_base + 0x68, 0x1, 0x0);
+	phy_update_bits(rphy->phy_base + 0x64, 0x80, 0x80);
+
 	return 0;
 }
 
