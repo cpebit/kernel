663eaeb2438713f16e840922c4ddb4bf5e03c096
Chiel Prins <chiel@cpeb.it>
2026-03-24 13:26:01 +0100

    Hack: Workaround for xmit thread hanging on shutdown in RTL8733 driver

--- drivers/net/wireless/rockchip_wlan/rtl8733bs/os_dep/linux/os_intfs.c
+++ drivers/net/wireless/rockchip_wlan/rtl8733bs/os_dep/linux/os_intfs.c
@@ -2486,7 +2486,7 @@ void rtw_stop_drv_threads(_adapter *padapter)
 			_rtw_up_sema(&padapter->xmitpriv.xmit_sema);
 			rtw_sdio_free_xmitbuf_sema_up(&padapter->xmitpriv);
 			RTW_INFO(FUNC_ADPT_FMT" stop tx sema\n", FUNC_ADPT_ARG(padapter));
-			rtw_thread_stop(padapter->xmitThread);
+			// rtw_thread_stop(padapter->xmitThread); // HACK
 			RTW_INFO(FUNC_ADPT_FMT" stop tx thread\n", FUNC_ADPT_ARG(padapter));
 			padapter->xmitThread = NULL;
 		}
