78884bea5b2777c855ac74ae9d9fd083e189098c
Chiel Prins <chiel@cpeb.it>
2026-08-26 18:39:50 +0200

    eh

--- drivers/phy/rockchip/phy-rockchip-inno-usb2.c
+++ drivers/phy/rockchip/phy-rockchip-inno-usb2.c
@@ -2888,7 +2888,7 @@ static int rv1103b_usb2phy_tuning(struct rockchip_usb2phy *rphy)
 	phy_update_bits(rphy->phy_base + 0x11c, GENMASK(4, 0), 0x17);
 
 	/* Set Tx HS eye height tuning to 3'b011(462 mV)*/
-	phy_update_bits(rphy->phy_base + 0x124, GENMASK(4, 2), (0x03 << 2));
+	phy_update_bits(rphy->phy_base + 0x124, GENMASK(4, 2), (0x07 << 2));
 
 	/* Bypass Squelch detector calibration */
 	phy_update_bits(rphy->phy_base + 0x1a4, GENMASK(7, 4), (0x01 << 4));
